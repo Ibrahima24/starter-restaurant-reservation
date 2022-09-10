12c22291a0e5e5bbaaf99a64ec86f7a13f784c4e
Ibrahima Diallo <Ibrahima1d@gmail.com>
2022-09-10 15:31:26 -0400

    completed backend files

--- back-end/src/db/seeds/00-reservations.js
+++ back-end/src/db/seeds/00-reservations.js
@@ -1,3 +1,9 @@
+const reservations = require("./00-reservations.json")
+
+/** inserts all reservations into the reservations table */
+
 exports.seed = function (knex) {
-  return knex.raw("TRUNCATE TABLE reservations RESTART IDENTITY CASCADE");
+  return knex
+    .raw("TRUNCATE TABLE reservations RESTART IDENTITY CASCADE")
+    .then(() => knex("reservations").insert(reservations));
 };
